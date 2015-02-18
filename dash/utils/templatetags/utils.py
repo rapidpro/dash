eb01d4ccbe48ca0a47cf69da25bac1d4837f7c49
Rowan Seymour <rowanseymour@gmail.com>
2015-02-18 13:22:42 +0200

    Added if_url template tag

---
+++ dash/utils/templatetags/utils.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import, unicode_literals
+
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def if_url(context, url_name, yes, no):
+    """
+    Example:
+        %li{ class:"{% if_url 'contacts.contact_read' 'active' '' %}" }
+    """
+    current = context['request'].resolver_match.url_name
+    return yes if url_name == current else no
