bdeb0af9156ca8beb0daa109083ac1c0af55b220
Rebecca <rebecca@caktusgroup.com>
2015-07-22 11:48:16 -0400

    Fix Flake8 warnings

--- dash_test_runner/settings.py
+++ dash_test_runner/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import warnings
+
 from django.utils.translation import ugettext_lazy as _
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -79,7 +81,6 @@ MIDDLEWARE_CLASSES = (
     'dash.orgs.middleware.SetOrgMiddleware',
 )
 
-import warnings
 warnings.filterwarnings('error', r"DateTimeField received a naive datetime",
                         RuntimeWarning, r'django\.db\.models\.fields')
 
