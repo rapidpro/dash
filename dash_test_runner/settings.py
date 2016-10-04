905882126a0df37c8b16043f889a74e070335b8e
Rowan Seymour <rowanseymour@gmail.com>
2016-10-04 15:56:38 +0200

    Added custom haml loaders which are compatible with django 1.9

--- dash_test_runner/settings.py
+++ dash_test_runner/settings.py
@@ -142,15 +142,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
@@ -183,17 +174,10 @@ STATICFILES_FINDERS = (
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'hamlpy.template.loaders.HamlPyFilesystemLoader',
-    'hamlpy.template.loaders.HamlPyAppDirectoriesLoader',
+    'dash.utils.haml.HamlFilesystemLoader',
+    'dash.utils.haml.HamlAppDirectoriesLoader',
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-    # 'django.template.loaders.eggs.Loader',
-)
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
 )
 
 AUTHENTICATION_BACKENDS = (
@@ -381,3 +365,5 @@ RESOURCES_DIR = os.path.join(PROJECT_DIR, '../resources')
 TESTFILES_DIR = os.path.join(PROJECT_DIR, '../testfiles')
 MEDIA_ROOT = os.path.join(PROJECT_DIR, '../media')
 MEDIA_URL = "/media/"
+
+TEMPLATE_DIRS = [os.path.join(PROJECT_DIR, '../templates')]
